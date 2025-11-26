f4b1f156c6e05da8997879c7bd785f382aaeb865
jaymehta2189 <jaymehta249@gmail.com>
2025-11-26 08:59:04 +0530

    Add Sub commit

---
+++ cal.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class cal{
+    public static void main(String args[]){
+        Scanner sc =new Scanner("");
+        int a ,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        int c=a+b;
+        int d=a-b;
+        System.out.println("add is "+c);
+        System.out.println("sub is "+d);
+
+    }
+}
