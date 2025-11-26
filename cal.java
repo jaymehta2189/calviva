c893ef64bf412bf71f19fc595c9bb2b00359b417
jaymehta2189 <jaymehta249@gmail.com>
2025-11-26 09:05:34 +0530

    Mul Div commit

--- cal.java
+++ cal.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class cal{
     public static void main(String args[]){
-        Scanner sc =new Scanner("");
+        Scanner sc =new Scanner(System.in);
         int a ,b;
         a=sc.nextInt();
         b=sc.nextInt();
@@ -10,6 +10,9 @@ public class cal{
         int d=a-b;
         System.out.println("add is "+c);
         System.out.println("sub is "+d);
-
+        c=a*b;
+        d=a/b;
+        System.out.println("mul is "+c);
+        System.out.println("div is "+d);
     }
 }
